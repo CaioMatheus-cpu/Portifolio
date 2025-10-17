e5b24f618d99c2c556f385a7994fb95f04e7b528
CaioMatheus-cpu <Caio.matheus.bz.silva@gmail.com>
2025-10-17 00:17:19 -0300

    nao

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "Portfolio",
     script : "npm",
-    args   : "start",
+    args   : "run dev",
     cwd    : "/home/rasp/Portifolio", // A instrução mais importante!
   }]
 }
